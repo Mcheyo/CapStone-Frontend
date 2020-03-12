316f97b99e637b33b8c0b39f7f3159a7cbf2675f
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-03-12 10:25:52 -0400

    fixed redirecting on sign up

--- src/components/SignUpMain.js
+++ src/components/SignUpMain.js
@@ -2,13 +2,16 @@ import React from 'react'
 import {connect} from 'react-redux'
 import SignUpClient from './SignUpClient'
 import SignUpDeveloper from './SignUpDeveloper'
+import {Redirect} from 'react-router-dom'
 const SignUp = props => { 
     
     return(
         <div>
              { props.value =="developer"?
              <SignUpDeveloper/>: 
-            <SignUpClient/>
+             props.value=="client"?
+            <SignUpClient/>: 
+            <Redirect to={'/'}/>
             
              }
         </div>
