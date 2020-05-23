8cb95e3f67302f6ab6aeeaa96671f8d874c045e3
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-05-23 12:52:14 -0400

    added form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,50 @@ import { connect } from 'react-redux'
 import {Button, Form, Card} from "react-bootstrap"
 import {Link} from "react-router-dom"
 
+
 export class Login extends Component {
+    state = { 
+        name: "", 
+        password: ""
+    }
     render() {
         return (
-            <div>
-                Adding Login Page
+            <div className="form-group"> 
+            <Card className="form-card">
+            <Form onSubmit={this.handleLoginSubmit}>
+                <Form.Group controlId="Username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username"
+                    placeholder="Enter username"  
+                    name="name"
+                    onChange={(event) => this.handleChange(event)}
+                    />
+                </Form.Group>
+
+                {/* <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        label="password"
+                        placeholder="password"
+                        name="password"
+                        onChange={(event) => this.handleChange(event)}
+                        // value={this.state.password}
+                    /> */}
+                    <Form.Text className="text">
+                        Not a Member? <Link to='/signup'>
+                        Sign up here
+                            </Link>
+                    </Form.Text>
+                {/* </Form.Group> */}
+
+                <Form.Group controlId="submit">
+                    <Button variant="primary" type="submit" size="lg" block>
+                    Submit
+                    </Button>
+                </Form.Group>
+            </Form>
+            </Card>
             </div>
         )
     }
