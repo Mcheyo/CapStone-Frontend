6f2268cc975bc9ccfdf2102e0c3d9bc842fd502a
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-06-07 16:01:47 -0400

    setting up route for market place

--- src/components/Marketplave.js
+++ src/components/Marketplave.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react'
 import {connect} from "react-redux"
 class Marketplace extends Component { 
 
-    
+    render(){ 
+        debugger
+        return(
+  <div>
+      testing 
+  </div>
+        )
+
+        
+    }
 }
 
 const mapStateToProps = (state) =>{ 
